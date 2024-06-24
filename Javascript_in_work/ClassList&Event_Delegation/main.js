d46ea733ba6e5db4e5d9793b3437a00ee8b27fed
Dmytro <hellcamaelle@gmail.com>
2024-06-24 12:24:05 +0300

    Add mini webpage with project about ClassList & Event Delegation

---
+++ Javascript_in_work/ClassList&Event_Delegation/main.js
@@ -0,0 +1,46 @@
+const btns = document.querySelectorAll('button'),
+        wrapper = document.querySelector('.btn-block');
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(1));
+// console.log(btns[1].classList.add('red'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+
+// btns.forEach( btn => {
+//     btn.addEventListener('click', () => {
+//         if(!btn.classList.contains('red')){
+//             btn.classList.add('red');
+//         } else{
+//             btn.classList.remove('red');
+//         }
+
+
+//         btn.classList.toggle('red');
+//     });
+// });
+
+// console.log(btns[0].className);
+
+
+wrapper.addEventListener('click', (event) =>{
+    // if(event.target && event.target.tagName == 'BUTTON'){
+    //     console.log('Hello');
+    // }
+    if(event.target && event.target.matches('button.red')){
+        console.log('Hello');
+    }
+    // if(event.target && event.target.classList.contains('blue')){
+    //     console.log('Hello');
+    // }
+});
+
+// btns.forEach( btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
